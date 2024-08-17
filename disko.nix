9006025e52e261a3294f59ab55dbab69b48d749b
VantagePointIncorporated <paradoxzss.vpi@gmail.com>
2024-08-17 12:07:17 -0400

    Update disko.nix

--- disko.nix
+++ disko.nix
@@ -1,36 +1,43 @@
 {
   disko.devices = {
     disk = {
-      my-disk = {
-        device = "/dev/sdb";
+      main = {
+        device = "/dev/nvme0n1";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              size = "500M";
               type = "EF00";
-              size = "256M";
               content = {
-               type = "filesystem";
+                type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
-
-            swap = {
-              size = "8G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
-              };
-            };
-
             root = {
-              size = "100%";
+              end = "-1G";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+              };
+            };
+            encryptedSwap = {
+              size = "10M";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            plainSwap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
               };
             };
           };
